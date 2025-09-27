2ca9a9cde71e6c2cf75d9db6a63454936c4d9a1e
aftb777 <aftaabmulla228@gmail.com>
2025-09-27 16:46:22 +0530

    Basic Grid

--- Grids/ContentView.swift
+++ Grids/ContentView.swift
@@ -8,7 +8,33 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            
+            Grid {
+                GridRow {
+                    Text("Exercise Name")
+                        .fontWeight(.bold)
+                        .font(.system(size: 15, weight: .semibold, design: .default))
+                        .frame(maxWidth: .infinity)
+                    Text("Reps")
+                        .fontWeight(.bold)
+                        .font(.system(size: 15, weight: .semibold, design: .default))
+                    Text("Sets")
+                        .fontWeight(.bold)
+                        .font(.system(size: 15, weight: .semibold, design: .default))
+                    Text("Completion")
+                        .fontWeight(.bold)
+                        .font(.system(size: 15, weight: .semibold, design: .default))
+                }
+                
+                Divider()
+                
+                GridRow {
+                    Text("Bench Press")
+                    Text("10")
+                    Text("3")
+                    Text("100%")
+                }
+            }
+            .padding()
         }
     }
 }
